6a129453400783e4fdc9b55ad59e878ddb5f9146
EOM-D <dayomi37@gmail.com>
2024-03-09 15:39:05 +0900

    UI 수정

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -14,6 +14,9 @@ public class Managers : MonoBehaviour
     InputManager _input = new InputManager();   // 그냥 필드
     public static InputManager Input { get { return Instance._input; } }
 
+    SceneManagerEx _scene = new SceneManagerEx();
+    public static SceneManagerEx Scene { get {  return Instance._scene; } }
+
     UIManager _ui = new UIManager();
     public static UIManager UI { get { return Instance._ui; } }
 
